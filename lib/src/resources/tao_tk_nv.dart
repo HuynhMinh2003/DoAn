79cd4f9992c7195a64d6076ca3be9e8cc2abb198
HuynhMinh2003 <hmvn2003@gmail.com>
2025-04-23 01:51:51 +0700

    day 4: xu li van de luu trong firebase voi cac du lieu dang ki va dang nhap

--- lib/src/resources/tao_tk_nv.dart
+++ lib/src/resources/tao_tk_nv.dart
@@ -1,7 +1,11 @@
 import 'package:do_an/src/blocs/auth_bloc.dart';
+import 'package:do_an/src/resources/dialog/loading_dialog.dart';
+import 'package:do_an/src/resources/dialog/msg_dialog.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class AddAccountStaffPage extends StatefulWidget {
   const AddAccountStaffPage({super.key});
@@ -14,16 +18,35 @@ class _AddAccountStaffPageState extends State<AddAccountStaffPage> {
   final TextEditingController _nameStaffController = TextEditingController();
   final TextEditingController _emailStaffController = TextEditingController();
   final TextEditingController _phoneStaffController = TextEditingController();
-  final TextEditingController _rollStaffController = TextEditingController();
 
   final AuthBloc _authBloc = AuthBloc();
+
+  String? _selectedRole;
+
+  List<String> _roleItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPositions();
+  }
+
+  void _fetchPositions() async {
+    final snapshot = await FirebaseDatabase.instance.ref().child("positions").get();
+    if (snapshot.exists) {
+      final data = Map<String, dynamic>.from(snapshot.value as Map);
+
+      setState(() {
+        _roleItems = data.values.map((e) => e.toString()).toList(); // lấy danh sách các "positions"
+      });
+    }
+  }
 
   @override
   void dispose() {
     _nameStaffController.dispose();
     _emailStaffController.dispose();
     _phoneStaffController.dispose();
-    _rollStaffController.dispose();
     _authBloc.dispose();
     super.dispose();
   }
@@ -81,28 +104,30 @@ class _AddAccountStaffPageState extends State<AddAccountStaffPage> {
                                     fontSize: 12.sp,
                                   ),
                                 ),
-                                SizedBox(
-                                  height: 10.h,
-                                ),
                                 _buildTextField(
                                   controller: _nameStaffController,
                                   label: 'Họ và tên:',
                                   stream: _authBloc.nameStaffStream,
                                 ),
                                 _buildTextField(
-                                  controller: _emailStaffController,
-                                  label: 'Số điện thoại:',
-                                  stream: _authBloc.emailStaffStream,
-                                ),
-                                _buildTextField(
                                   controller: _phoneStaffController,
-                                  label: 'Email:',
+                                  label: 'Số điện thoại:',
                                   stream: _authBloc.phoneStaffStream,
                                 ),
                                 _buildTextField(
-                                  controller: _rollStaffController,
-                                  label: 'Vai trò:',
-                                  stream: _authBloc.rollStaffStream,
+                                  controller: _emailStaffController,
+                                  label: 'Email:',
+                                  stream: _authBloc.emailStaffStream,
+                                ),
+                                buildFilterDropdown(
+                                  label: "Vai trò",
+                                  items: _roleItems,
+                                  selectedValue: _selectedRole,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _selectedRole = value;
+                                    });
+                                  },
                                 ),
                                 SizedBox(
                                   height: 40.h,
@@ -113,7 +138,9 @@ class _AddAccountStaffPageState extends State<AddAccountStaffPage> {
                                       child: SizedBox(
                                         height: 60.h,
                                         child: ElevatedButton(
-                                          onPressed: () {},
+                                          onPressed: () {
+                                            _onSignUpStaffClicked();
+                                          },
                                           style: ElevatedButton.styleFrom(
                                             backgroundColor:
                                             const Color(0xFF2D80F8),
@@ -218,71 +245,94 @@ class _AddAccountStaffPageState extends State<AddAccountStaffPage> {
     );
   }
 
-}
-
-class FilterDropdown extends StatefulWidget {
-  final String label;
-
-  const FilterDropdown({super.key, required this.label});
-
-  @override
-  State<FilterDropdown> createState() => _FilterDropdownState();
-}
-
-class _FilterDropdownState extends State<FilterDropdown> {
-  final TextEditingController _controller = TextEditingController();
-
-  @override
-  void dispose() {
-    _controller.dispose(); // đừng quên xoá controller
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final isLandscape = size.height < size.width;
-
+  Widget buildFilterDropdown({
+    required String label,
+    required List<String> items,
+    required String? selectedValue,
+    required ValueChanged<String?> onChanged,
+  }) {
     return Padding(
-      padding: EdgeInsets.only(bottom: isLandscape ? 50.h : 50.h),
-      //  padding thay đổi theo màn hình
-      child: Row(
-        children: [
-          SizedBox(
-            width: isLandscape ? 50.w : 100.w,
-            child: Text(
-              widget.label,
-              style: TextStyle(
-                fontFamily: "Oswald",
-                fontWeight: FontWeight.w700,
-                fontSize: isLandscape ? 8.sp : 16.sp,
+      padding: EdgeInsets.fromLTRB(0.w, 30.h, 0.w, 8.h),
+      child: SizedBox(
+        height: 60.h,
+        child: Container(
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton2<String>(
+              isExpanded: true,
+              hint: Text(
+                label+":",
+                style: TextStyle(color: Colors.black),
+              ),
+              items: items.map((item) {
+                return DropdownMenuItem(
+                  value: item,
+                  child: Text(
+                    item,
+                  ),
+                );
+              }).toList(),
+              value: selectedValue,
+              onChanged: onChanged,
+              buttonStyleData: ButtonStyleData(
+                height: 40.h,
+                padding: EdgeInsets.symmetric(vertical:2.h,horizontal: 10.w),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.r),
+                  border: Border.all(color: Color(0xe2707070)),
+                  color: Color(0xFFF7FEFF),
+                ),
+              ),
+              dropdownStyleData: DropdownStyleData(
+                maxHeight: 200.h,
+                width: 172.w,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.r),
+                  color: Color(0xFFF7FEFF),
+                ),
+                elevation: 4,
+              ),
+              menuItemStyleData: MenuItemStyleData(
+                height: 40.h,
+                padding: EdgeInsets.symmetric(horizontal: 10.w),
               ),
             ),
           ),
-          SizedBox(width: 2.w),
-          Expanded(
-            child: Container(
-              height: 30.h,
-              padding: EdgeInsets.symmetric(horizontal: 2.w),
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(color: Colors.black26),
-                ),
-              ),
-              child: TextField(
-                controller: _controller,
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  isDense: true,
-                ),
-                onChanged: (value) {
-                  // có thể trigger search logic hoặc UI cập nhật ở đây
-                },
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
+  }
+
+  _onSignUpStaffClicked(){
+    if (_selectedRole == null) {
+      MsgDialog.showMsgDialog(context, "Lỗi", "Vui lòng chọn vai trò.");
+      return;
+    }
+    var isValidStaff = _authBloc.isValidStaffSignUp(
+      _nameStaffController.text,
+      _emailStaffController.text,
+      _phoneStaffController.text,
+      _selectedRole!,
+    );
+
+    if (isValidStaff) {
+      // loading dialog
+      LoadingDialog.showLoadingDialog(context, 'Đang tải ...');
+
+      // create user
+      _authBloc.signUpStaff(
+        name: _nameStaffController.text,
+        email: _emailStaffController.text,
+        phone: _phoneStaffController.text,
+        position: _selectedRole!,
+        onSuccess: () {
+          LoadingDialog.hideLoadingDialog(context);
+          MsgDialog.showMsgDialog(context, "Tạo tài khoản thành công!", "Tài khoản đã được tạo.");
+        },
+        onRegisterError: (msg) {
+          LoadingDialog.hideLoadingDialog(context);
+          MsgDialog.showMsgDialog(context, "Tạo tài khoản thất bại !", msg);
+        },
+      );
+    }
   }
 }
