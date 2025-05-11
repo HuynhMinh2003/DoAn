bca47ff14fedc94032791c6db957c09da5b835b3
HuynhMinh2003 <hmvn2003@gmail.com>
2025-05-11 14:57:00 +0700

    day 20+21: fix 1 so loi tiem an cua cac giao dien khac, bo sung truong thong tin nhan vien, cu dan

---
+++ lib/src/resources/ds_nhanvien_mobile_page.dart
@@ -0,0 +1,7 @@
+import 'package:do_an/src/models/staffs.dart';
+import 'package:flutter/material.dart';
+
+Future<void> exportStaffsToExcel(List<Staff> staffs) async {
+  Text('Chức năng này không hỗ trợ trên mobile.');
+
+}
